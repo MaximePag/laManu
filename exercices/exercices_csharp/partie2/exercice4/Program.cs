c74471542f84fcef8c9a9e74675feb44f55e280a
MaximePag <max_p@live.fr>
2021-01-15 14:36:36 +0100

    15012021

--- exercices/exercices_csharp/partie2/exercice4/Program.cs
+++ exercices/exercices_csharp/partie2/exercice4/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace exercice4
+namespace exercice2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Entrez votre identifiant");
+            string login = Console.ReadLine();
+            Console.WriteLine("Entrez votre mot de passe");
+            string password = Console.ReadLine();
+            switch (login)
+            {
+                case "maxpag":
+                    switch (password)
+                    {
+                        case "gneugneu":
+                            Console.WriteLine("Bienvenu à la Manu");
+                            break;
+                        default:
+                            Console.WriteLine("Identifiant ou mot de passe incorrect");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Identifiant ou mot de passe incorrect");
+                    break;
+            }
         }
     }
 }
---
+++ exercices/exercices_csharp/partie2/exercice5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace exercice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choisissez un mois de l'année.");
+            string month = Console.ReadLine();
+            if ((month == "Mars") || (month == "Avril") || (month == "Mai"))
+            {
+                Console.WriteLine("La saison du mois saisi est le Printemps.");
+            }
+            else if ((month == "Juin") || (month == "Juillet") || (month == "Août"))
+            {
+                Console.WriteLine("La saison du mois saisi est l'Eté.");
+            }
+            else if ((month == "Septembre") || (month == "Octobre") || (month == "Décembre"))
+            {
+                Console.WriteLine("La saison du mois saisi est l'Automne.");
+            }
+            else if ((month == "Janvier") || (month == "Février") || (month == "Mars"))
+            {
+                Console.WriteLine("La saison du mois saisi est l'Hiver.");
+            }
+            else
+            {
+                Console.WriteLine("Le mois saisi est inconnu ou mal orthographié.");
+            }
+        }
+    }
+}
