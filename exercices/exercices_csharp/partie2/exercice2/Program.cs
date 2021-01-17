034c1aee83c93685c39e8ef595f9ce4e67f0c970
MaximePag <max_p@live.fr>
2021-01-17 19:34:26 +0100

    17012021

--- exercices/exercices_csharp/partie2/exercice2/Program.cs
+++ exercices/exercices_csharp/partie2/exercice2/Program.cs
@@ -7,14 +7,27 @@ namespace exercice2
         static void Main(string[] args)
         {
             Console.WriteLine("Quel âge avez-vous ?");
-            int age = Convert.ToInt32(Console.ReadLine());
-            if( age >= 18)
+            string age = Console.ReadLine();
+            int verifyAge;
+            bool isNumber = int.TryParse(age, out verifyAge);
+            if (isNumber)
             {
-                Console.WriteLine("Vous avez " + age + "ans, vous êtes donc majeur.e.");
+                if (verifyAge >= 18 && verifyAge <= 130)
+                {
+                    Console.WriteLine("Vous avez " + age + "ans, vous êtes donc majeur.e.");
+                }
+                else if (verifyAge < 18 && verifyAge > 0)
+                {
+                    Console.WriteLine("Vous avez " + age + "ans, vous êtes donc mineur.e.");
+                }
+                else
+                {
+                    Console.WriteLine("Ce n'est pas possible.");
+                }
             }
             else
             {
-                Console.WriteLine("Vous avez " + age + "ans, vous êtes donc mineur.e.");
+                Console.WriteLine("Âge invalide, veuillez écrire un nombre.");
             }
         }
     }
---
+++ exercices/exercices_csharp/partie4/exercice2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lanquages = new List<string> { <languages>, "Ana", "Felipe" };
+        }
+    }
+}
