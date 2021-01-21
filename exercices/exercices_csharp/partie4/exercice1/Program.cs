0ffb936a72d2a746411ce753afcb61536bcc99b6
MaximePag <max_p@live.fr>
2021-01-21 11:32:02 +0100

    21012021

--- exercices/exercices_csharp/partie4/exercice1/Program.cs
+++ exercices/exercices_csharp/partie4/exercice1/Program.cs
@@ -6,7 +6,7 @@ namespace exercice1
     {
         static void Main(string[] args)
         {
-            string[] week = { "Lundi", "Mardi", "Mercredi", "Jedi", "Vendredi", "Samedi" };
+            string[] week = new string[7]{ "Lundi", "Mardi", "Mercredi", "Jedi", "Vendredi", "Samedi", "" };
             Console.WriteLine("Affichage de l'indice 1 et 4 du tableau week");
             Console.WriteLine(week[1]);
             Console.WriteLine(week[4]);
@@ -16,9 +16,8 @@ namespace exercice1
             Console.WriteLine("Edition du quatrième élément du tableau week");
             week[3] = "Jeudi";
             Console.WriteLine(week[3]);
-            Console.WriteLine("Insertion du 7 élément Dimanche dans le tableau newWeek");
-            string[] newWeek = { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
-            foreach (string days in newWeek)
+            week[6] = "Dimanche";
+            foreach (string days in week)
             {
                 Console.Write(days + " ");
             }
