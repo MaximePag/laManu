61975320b2ad5b030c46fb2bd8811d6a28f0dc71
MaximePag <max_p@live.fr>
2021-01-22 09:06:16 +0100

    22012021

--- exercices/exercices_csharp/partie4/exercice3/Program.cs
+++ exercices/exercices_csharp/partie4/exercice3/Program.cs
@@ -8,21 +8,25 @@ namespace exercice1
         static void Main(string[] args)
         {
             // Créer une liste numbers en utilisant using System.collections.generic
-            List<int> numbers = new List<int>();
-            numbers.Add(69);
-            numbers.Add(96);
-            numbers.Add(42);
-            numbers.Add(3);
-            numbers.Add(9);
-            int sum;
+            List<int> numbers = new List<int>() { 69, 96, 42, 3, 9 };
+            int count = 1;
+            int sum = 0;
+            string calc = "";
             Console.WriteLine("Calculer la somme des nombres de la liste \r\n");
-            for(int i = 0; i < numbers.Count - 1; i++)
+            foreach( int number in numbers)
             {
-                Console.WriteLine(numbers[i] + " + " + numbers[i + 1]);
-                sum = numbers[i] + numbers[i + 1];
-                Console.WriteLine(sum);
-                numbers[i + 1] = sum;
+                sum += number;
+                if(count == numbers.Count)
+                {
+                    calc += number;
+                }
+                else
+                {
+                    calc += number + " + ";
+                }
+                count++;
             }
+            Console.WriteLine($"{calc} = {sum}");
         }
     }
 }
--- exercices/exercices_csharp/partie4/exercice4/Program.cs
+++ exercices/exercices_csharp/partie4/exercice4/Program.cs
@@ -9,46 +9,76 @@ namespace exercice1
         {
             // Créer une liste numbers en utilisant using System.collections.generic
             List<int> numbers = new List<int>();
-            string response = "";
-            int number;
-            bool end;
-            do
+            string answer = "y";
+            while( answer != "n")
             {
-                Console.WriteLine("\r\nEcrivez un nombre à multiplier. Ecrivez 'stop' si vous voulez arrêter.");
-                response = Console.ReadLine();
-                bool isNumber = int.TryParse(response, out number);
-                if (isNumber)
+                Console.WriteLine("Veuillez ajouter un nombre à la liste :");
+                numbers.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Voulez vous ajouter un nombre à la liste ? (y/n)");
+                answer = Console.ReadLine();
+            }
+
+            int count = 1;
+            int result = 1;
+            string calc = "";
+            Console.WriteLine("Calcul du produit des nombres de la liste \r\n");
+            foreach (int number in numbers)
+            {
+                result *= number;
+                if (count == numbers.Count)
                 {
-                    numbers.Add(number);
-                    Console.WriteLine("\r\nJ'ai ajouté " + number + " dans la liste");
-                    end = false;
-                }
-                else if ((response == "stop") && (numbers.Count < 2))
-                {
-                    Console.WriteLine("\r\nIl faut au minimum deux nombres à multiplier.");
-                    end = false;
-                }
-                else if (response != "stop")
-                {
-                    Console.WriteLine("\r\nJe n'ai pas compris.");
-                    end = false;
+                    calc += number;
                 }
                 else
                 {
-                    end = true;
+                    calc += number + " x ";
                 }
+                count++;
             }
-            while (end == false);
-            int result;
-            int[] numbersArray = numbers.ToArray();
-            Console.WriteLine("\r\nLe résultat de la multiplication est :");
-            for (int i = 0; i < numbersArray.Length - 1; i++)
-            {
-                Console.WriteLine(numbersArray[i] + " x " + numbersArray[i + 1]);
-                result = numbersArray[i] * numbersArray[i + 1];
-                Console.WriteLine(result);
-                numbersArray[i + 1] = result;
-            }
+            Console.WriteLine($"{calc} = {result}");
+
+            //// Créer une liste numbers en utilisant using System.collections.generic
+            //List<int> numbers = new List<int>();
+            //string response = "";
+            //int number;
+            //bool end;
+            //do
+            //{
+            //    Console.WriteLine("\r\nEcrivez un nombre à multiplier. Ecrivez 'stop' si vous voulez arrêter.");
+            //    response = Console.ReadLine();
+            //    bool isNumber = int.TryParse(response, out number);
+            //    if (isNumber)
+            //    {
+            //        numbers.Add(number);
+            //        Console.WriteLine("\r\nJ'ai ajouté " + number + " dans la liste");
+            //        end = false;
+            //    }
+            //    else if ((response == "stop") && (numbers.Count < 2))
+            //    {
+            //        Console.WriteLine("\r\nIl faut au minimum deux nombres à multiplier.");
+            //        end = false;
+            //    }
+            //    else if (response != "stop")
+            //    {
+            //        Console.WriteLine("\r\nJe n'ai pas compris.");
+            //        end = false;
+            //    }
+            //    else
+            //    {
+            //        end = true;
+            //    }
+            //}
+            //while (end == false);
+            //int result;
+            //int[] numbersArray = numbers.ToArray();
+            //Console.WriteLine("\r\nLe résultat de la multiplication est :");
+            //for (int i = 0; i < numbersArray.Length - 1; i++)
+            //{
+            //    Console.WriteLine(numbersArray[i] + " x " + numbersArray[i + 1]);
+            //    result = numbersArray[i] * numbersArray[i + 1];
+            //    Console.WriteLine(result);
+            //    numbersArray[i + 1] = result;
+            //}
         }
     }
 }
