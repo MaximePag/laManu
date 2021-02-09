63061bb4c48f8ae4ab10d5fb12df4af85b8a6af9
MaximePag <max_p@live.fr>
2021-02-09 14:15:51 +0100

    09022021

--- exercices/exercices_js_2/Partie1/exercice3.js
+++ exercices/exercices_js_2/Partie1/exercice3.js
@@ -1,11 +1,9 @@
-var age = prompt('Quel est votre âge ?')
-if (age < 18 && age > 0){
+let age = prompt('Quel est votre âge ?');
+
+if (age < 18 && age > 0) {
     alert('Vous êtes mineur');
-}
-if (age >= 18 && age < 125){
+} else if (age >= 18 && age < 125) {
     alert('Vous êtes majeur');
+} else {
+    alert('Veuillez entrer un âge correct.');
 }
-else{
-    alert('Ce n\'est pas possible');
-}
-alert('Bonjour ' + username);
